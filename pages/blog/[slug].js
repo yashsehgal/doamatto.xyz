68266a2387b6ea9c623c9a4cca79d24e47b7e373
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-14 08:03:45 -0800

    chore: configured blogging

---
+++ pages/blog/[slug].js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+/**
+  * Major thanks to this Dev.to post:
+  * https://dev.to/sagar/building-a-blog-with-next-js-253
+  */
+
+function BlogPostPage(props) {
+    return(
+        <div>
+            <h1>{props.blog.title}</h1>
+            <section dangerouslySetInnerHTML={{
+                __html: props.blog.content
+            }} />
+        </div>
+    )
+}
+
+export async function getStaticProps(context) {
+    const fs = require('fs')
+    const html = require('remark-html')
+    const unified = require('unified')
+    const markdown = require('remark-parse')
+    const matter = require('gray-matter')
+    const slug = context.params.slug
+    const path = `${process.cwd()}/posts/${slug}.md`
+    const rawContent = fs.readFileSync(path, {
+        encoding: 'utf-8',
+    })
+    const { data, content } = matter(rawContent)
+    const result = await unified()
+        .use(markdown)
+        .use(html)
+        .process(content)
+    
+    return {
+        props: {
+            blog: {
+                ...data,
+                content: result.toString(),
+            }
+        }
+    }
+}
+
+export async function getStaticPaths(context) {
+    const fs = require('fs')
+    const path = `${process.cwd()}/posts`
+    const files = fs.readdirSync(path, 'utf-8')
+    const markdownFileNames = files
+        .filter((fn) => fn.endsWith('.md'))
+        .map((fn) => fn.replace('.md', ''))
+
+    return {
+        paths: markdownFileNames.map((fileName) => {
+            return {
+                params: {
+                    slug: fileName,
+                },
+            }
+        }),
+        fallback: false,
+    }
+}
+
+export default BlogPostPage
