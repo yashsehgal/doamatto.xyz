37c944d47bbfcf4e124991c3801c84d0b2bbb415
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-15 14:37:35 -0800

    chore: worked on footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,11 @@
 import styles from './footer.module.scss'
 
-export default function Footer() {
+export default function Footer() {    
     return (
-        <footer>
+        <footer className={styles.footer}>
             <p>
                 &copy; 2019-2021 doamatto. <br/>
-                <a href="">View this page's source code.</a> <br/>
+                <a href={`https://github.com/doamatto/doamatto.xyz`}>View this page's source code.</a> <br/>
                 <a href="/imprint">Imprint.</a> <a href="/privacy">Privacy.</a> <a href="/sitemap">Sitemap.</a>
             </p>
         </footer>
