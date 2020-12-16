24b8fbc456248f604e0e6e8561c382d1fa8d0b19
Matt Ronchetto <hello@doamatto.xyz>
2020-12-16 10:52:45 -0800

    fix: imported properly

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import NextSeo from 'next-seo'
+import {NextSeo} from 'next-seo'
 import dynamic from 'next/dynamic'
 import styles from './[slug].module.scss'
 
