2ea6242edaeb5783d3413dbc0a7f98ed88d10cba
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-16 20:59:37 -0800

    fix: post not populating (resolves #42)

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    webpack: (config) => {
+        config.node = {
+            fs: 'empty',
+            net: 'empty',
+            tls: 'empty'
+        }
+        return config
+    },
+    webpackDevMiddleware: config => {
+        return config
+    },
+}
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,42 +1,72 @@
 import React from 'react'
 import Link from 'next/link'
-import { GetStaticProps } from 'next'
-import { stringify } from 'querystring'
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
+const styles = require('./blog.module.scss')
+import {GetStaticProps} from 'next'
 
-export default function IndexPage(props) {
+export default function IndexPage({postData}:{
+  postData: {
+    title: string
+    date: string
+    description: string
+    slug: string
+    id: string
+  }[]
+}) {
   return(
     <div>
       <h1>Posts</h1>
       <ul>
-        <li key={props.blogs.id}>
-          <Link href={`/blog/${props.blogs.slug}`}>
-            <a>{props.blogs.date}<br />{props.blogs.title}</a>
-          </Link>
-        </li>
+        {postData.map(({title,date,description,slug,id}) => (
+          <li id={id}>
+            <Link href={`/blog/${slug}`}>
+              <a>
+                {date}<br />
+                {title}<br />
+                {description}<br />
+                </a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
-    const fs = require("fs")
-    const matter = require("gray-matter")
-    const { v4: uuid } = require("uuid")
-    const files = fs.readdirSync(`${process.cwd()}/posts`, "utf-8")
-    const blogs = await files
-      .filter((fn: string) => fn.endsWith(".md"))
-      .map((fn: any) => {
-        const path = `${process.cwd()}/posts/${fn}`
-        const rawContent = fs.readFileSync(path, {
-          encoding: "utf-8",
-        })
-        const { data } = matter(rawContent)
-  
-        return { ...data, id: uuid() }
-    })
-    return {
-      props: {
-          blogs
-      }
+export const getStaticProps: GetStaticProps = async () => {
+  const postData = getSortedPostsData()
+  return {
+    props: {
+      postData
     }
+  }
+}
+
+export function getSortedPostsData() {
+  const fileNames = fs.readdirSync(path.join(process.cwd(), 'posts'))
+  const postData = fileNames.map(fileName => {
+    const id = fileName.replace(/\.md$/, '')
+    const fullPath = path.join(process.cwd(), 'posts', fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const matterResult = matter(fileContents)
+    return {
+      id,
+      ...(matterResult.data as {
+        title: string
+        date: string
+        description: string
+        slug: string
+        id: string
+      })
+    }
+  })
+  return postData.sort((a,b) => {
+    if(a.date < b.date) {
+      return 1
+    } else {
+      return -1
+    }
+  })
 }
