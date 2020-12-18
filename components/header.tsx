9a80e211fc0b56eef830922ba864f2ba5c0b1454
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-17 20:11:12 -0800

    chore: switch to typescript

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,8 @@
-import styles from './header.module.scss'
+const styles = require('./header.module.scss')
 
 // <Header title="Insert title here" />
 
-export default function Header ({ title }) {
+export default function Header ({ title }: {title: string}) {
   return (
         <div className={styles.header}>
             <h1>{title}</h1>
