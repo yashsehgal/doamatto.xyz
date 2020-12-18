8700ba62ef1be132b10369a47859336b34ec0946
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-17 20:28:04 -0800

    chore: minimizations

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,9 @@
-const { src, parallel } = require('gulp')
+const { src } = require('gulp')
 const sass = require('gulp-sass')
-const es = require('gulp-eslint')
-
 sass.compiler = require('node-sass')
 
-exports.default = parallel(eslint, sassCompile)
-exports.compileJS = eslint
-exports.lint = eslint
+exports.default = sassCompile
 exports.compileSASS = sassCompile
-
-function eslint () {
-  return src(['**/*.js', '**/*.ts', '**/*.tsx', '!node_modules/**'])
-    .pipe(es({ fix: true }))
-    .pipe(es.format())
-    .pipe(es.failAfterError())
-}
 
 function sassCompile () {
   return src('**/*.scss', '!node_modules/**')
