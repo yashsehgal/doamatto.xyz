de82f00967a55e7bccfdac81c9de8f6c634497af
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-17 20:14:50 -0800

    fix: switch to better camelcase

--- components/blm-banner.js
+++ components/blm-banner.js
@@ -1,6 +1,6 @@
 import styles from './blm-banner.module.scss'
 
-export default function blmBanner () {
+export default function BLMBanner () {
   return (
         <a className={styles.banner} href="https://support.eji.org/give/153413/#!/donation/checkout" title="Support the Equal Justice Initiative">
             Black Lives Matter. Support&nbsp;the&nbsp;Equal&nbsp;Justice&nbsp;Initiative.
