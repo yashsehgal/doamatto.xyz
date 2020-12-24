6d1bb5a5b08703278388a4a98bd194ea92c6cdb6
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-24 08:13:08 -0800

    fix: links in footer for langs

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,18 @@
 const styles = require('./footer.module.scss')
+import {useRouter} from 'next/router'
 import Link from 'next/link'
 
 export default function Footer () {
+  const router = useRouter()
   return (
         <footer className={styles.footer}>
             <p>
                 &copy; 2019-2021 doamatto. <br/>
                 <a href={'https://github.com/doamatto/doamatto.xyz'}>View this page&apos;s source code.</a> <br/>
                 <a href="/imprint">Imprint.</a> <a href="/privacy">Privacy.</a> <a href="/sitemap">Sitemap.</a> <br/>
-                <p>
-                  View this page in: <Link href={`/${window.location.pathname}`} locale={'en'}><a>English</a></Link> <Link href={`/fr/${window.location.pathname}`} locale={'fr'}><a>Français</a></Link><Link href={`/jp/${window.location.pathname}`} locale={'jp'}><a>日本語</a></Link>
-                </p>
+                {/*<p>
+                  View this page in: <Link href={`/${router.pathname}`} locale={'en'}><a>English</a></Link> <Link href={`/fr/${router.pathname}`} locale={'fr'}><a>Français</a></Link><Link href={`/jp/${router.pathname}`} locale={'jp'}><a>日本語</a></Link>
+                </p>*/}
             </p>
         </footer>
   )
