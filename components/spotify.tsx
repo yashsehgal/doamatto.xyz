76e137c5612edb012e66e96d7d34b9a203f69a94
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2020-12-30 14:37:22 -0800

    chore: made spotify component local

---
+++ components/spotify.tsx
@@ -0,0 +1,191 @@
+const React = require('react')
+import * as https from 'https'
+import styled from 'styled-components'
+
+const Widget = styled.div`
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 20px 25px;
+position: absolute;
+width: 437px;
+height: 168px;
+background: #FBFBFB;
+@media(prefers-color-scheme: 'dark') {background: #353535}
+border-radius: 10px;
+
+.albumArt {
+  position: static;
+  width: 128px;
+  height: 128px;
+  left: 25px;
+  top: 20px;
+  border-radius: 5px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: 15px 0px;
+}
+
+.title {
+  position: absolute;
+  width: 198px;
+  height: 48px;
+  left: 0px;
+  top: 12.5px;
+  
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 2.25em;
+  line-height: 3.5em;
+  display: flex;
+  align-items: center;
+  
+  color: #000000;
+}
+
+.artist {
+  position: absolute;
+  width: 198px;
+  height: 19px;
+  left: 0px;
+  top: 60.25px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+
+  color: #000000;
+}
+
+.open {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 7px 46px;
+  
+  position: static;
+  width: 189px;
+  height: 30px;
+  left: 0px;
+  top: 85.5px;
+  
+  background: #1ED760;
+  border-radius: 10px;
+  color: #000000;
+}
+
+.logo {
+  width: 32px;
+  height: 32px;
+  .st0 {
+    fill: black;
+    @media (prefers-color-scheme: 'dark') {
+      fill: white;
+    }
+  }
+}
+`
+
+export default function Main() {
+  return console.error("You need to specify one of the widgets to use this");
+}
+
+const spotifyLogo = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+      <path className="st0" d="M16 0C7.16315 0 0 7.16315 0 16C0 24.8368 7.16315 32 16 32C24.8368 32 32 24.8368 32 16C32 7.16424 24.8368 0.00109178 16 0ZM23.3378 23.0758C23.0507 23.5475 22.436 23.6948 21.9666 23.4077C18.2098 21.1117 13.4802 20.5931 7.91102 21.865C7.37387 21.9884 6.83889 21.6521 6.71662 21.115C6.59324 20.5778 6.92842 20.0429 7.46667 19.9206C13.561 18.5286 18.7895 19.1279 23.007 21.7045C23.4765 21.9939 23.6261 22.6063 23.3378 23.0758V23.0758ZM25.2954 18.7207C24.934 19.3081 24.1654 19.4915 23.5791 19.1312C19.2797 16.488 12.7225 15.7227 7.63589 17.2665C6.97646 17.4663 6.2799 17.094 6.07902 16.4356C5.88031 15.7762 6.25261 15.0807 6.91095 14.8798C12.7214 13.1166 19.9457 13.9704 24.8838 17.0055C25.4712 17.3669 25.6568 18.1344 25.2954 18.7207ZM25.4635 14.1833C20.306 11.1208 11.7988 10.8392 6.87492 12.3327C6.08448 12.5729 5.24817 12.1264 5.00908 11.3359C4.76998 10.5455 5.21542 9.70918 6.00696 9.46899C11.6591 7.7538 21.0538 8.08461 26.9909 11.6089C27.7017 12.0314 27.9353 12.9496 27.5139 13.6592C27.0936 14.3711 26.1732 14.6058 25.4635 14.1833Z"/>
+    </svg>
+`
+
+export function SpotifyAlbumWidget({albumId, token}:{albumId: string, token: string}) {
+    https.get(`https://api.spotify.com/v1/albums/${albumId}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    }, (res: any) => {
+      let data: any = '';
+      res.on('data', (chunk: any) => {
+        data += chunk;
+      })
+      res.on('end', () => {
+        let d = JSON.parse(data)
+        var albumArt = d.album.images[1].url,
+            albumName = d.album.name,
+            title = d.name,
+            artist = d.artists[0].name,
+            url = d.external_urls.spotify
+        return (
+          <Widget>
+            <img src={albumArt}
+              alt={albumName}
+              width="128"
+              height="128"
+              className="albumArt"
+            />
+            <div>
+              <p className="title">{title}</p>
+              <p className="artist">{artist}</p>
+              <a href={url} title={`Listen to ${title} on Spotify`} className="open">Open in Spotify</a>
+            </div>
+            <img src={spotifyLogo}
+              alt="Powered by Spotify"
+              width="32"
+              height="32"
+              className="logo"
+            />
+          </Widget>
+        )
+      })
+    }).on('error', (err: string) => { return console.error(err) })
+}
+
+export function SpotifyTrackWidget({albumId, token}:{albumId: string, token: string}) {
+    https.get(`https://api.spotify.com/v1/tracks/${albumId}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    }, (res: any) => {
+      let data: any = '';
+      res.on('data', (chunk: any) => {
+        data += chunk;
+      })
+      res.on('end', () => {
+        let d = JSON.parse(data)
+        var albumArt = d.images[1].url,
+            albumName = d.name,
+            title = d.name,
+            artist = d.artists[0].name,
+            url = d.external_urls.spotify
+        return (
+          <Widget>
+            <img src={albumArt}
+                alt={albumName}
+                width="128"
+                height="128"
+                className="albumArt"
+            />
+            <div>
+              <p className="title">{title}</p>
+              <p className="artist">{artist}</p>
+              <a href={url} title={`Listen to ${title} on Spotify`} className="open">Open in Spotify</a>
+            </div>
+            <img src={spotifyLogo}
+                alt="Powered by Spotify"
+                width="32"
+                height="32"
+                className="logo"
+            />
+          </Widget>
+        )
+      })
+    }).on('error', (err: string) => { console.error(err) })
+}
