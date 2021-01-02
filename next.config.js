8ad02e4059b74a5abbbb97afed71cf5395fb98db
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-02 10:58:21 -0800

    chore: setup redirects for legacy posts (resolves #75)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { getRedirectStatus } = require('next/dist/lib/load-custom-routes')
+
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/
 })
@@ -22,5 +24,60 @@ module.exports = {
   },
   webpackDevMiddleware: config => {
     return config
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/privacy/southpine-wrappers',
+        destination: '/projects/southpine-wrappers#privacy-statement',
+        permanent: true
+      },
+      {
+        source: "/updates%20projects/:date*/aas",
+        destination: "/projects/always-amazon-smiling",
+        permanent: true
+      },
+      {
+        source: "/updates%20projects/:date*/southnode",
+        destination: "/projects/southpine-wrappers",
+        permanent: true
+      },
+      {
+        source: "/updates%20projects/:date*/southpy",
+        destination: "/projects/southpine-wrappers",
+        permanent: true
+      },
+      {
+        source: "/updates%20projects/:date*/5mloading",
+        destination: "/projects/5mloading",
+        permanent: true
+      },
+      {
+        source: "/updates/mello/:slug*",
+        destination: "/projects/mello",
+        permanent: true
+      },
+      {
+        source: "/updates/swift/:slug*",
+        destination: "/projects/swift",
+        permanent: true
+      },
+      {
+        source: "/updates/hex/:slug*",
+        destination: "/projects/mono",
+        permanent: true
+      },
+      {
+        source: "/updates/nano/:slug*",
+        destination: "/projects/nano",
+        permanent: true
+      },
+
+      {
+        source: "/updates%20projects/:date*/ppg",
+        destination: "https://github.com/doamatto/phone-passcode-gen",
+        permanent: true
+      },
+    ]
+  } 
 }
