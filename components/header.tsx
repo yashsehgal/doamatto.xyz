52913637bcd2e3a206c55125f19be817df52177f
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-04 08:12:57 -0800

    chore: add subtitle to header (resolves #85)

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,19 @@ const styles = require('./header.module.scss')
 
 // <Header title="Insert title here" />
 
-export default function Header ({ title }: {title: string}) {
-  return (
+export default function Header ({ title, subtitle }: {title: string, subtitle?: string}) {
+  if (subtitle === undefined) {
+    return (
         <div className={styles.header}>
             <h1>{title}</h1>
         </div>
-  )
+    )
+  } else {
+    return (
+      <div className={styles.header}>
+          <h1 className={styles.title}>{title}</h1>
+          <h3 className={styles.subtitle}>{subtitle}</h3>
+      </div>
+    )
+  }
 }
