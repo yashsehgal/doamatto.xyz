d81734d737b863291fc3aa6d1af43a7fb68d7609
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-04 08:41:15 -0800

    chore: new fetch and display method

--- components/spotify.tsx
+++ components/spotify.tsx
@@ -1,6 +1,6 @@
 const React = require('react')
 import * as https from 'https'
-import request from 'request'
+import fetch from 'isomorphic-unfetch'
 import querystring from 'querystring'
 
 import {default as Logo} from './logos/spotify'
@@ -18,39 +18,25 @@ const {
 } = process.env
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
-const access_token = getAccessToken()
-console.log(access_token)
-console.log('=== ==== ===')
-
-export function getAccessToken() {
-  const data = querystring.stringify({
-    grant_type: refresh_token,
-    refresh_token: refresh_token
-  })
-
-  const options = {
+export async function getAccessToken() {
+  const res = await fetch("https://accounts.spotify.com/api/token", {
     method: 'POST',
-    url: 'https://accounts.spotify.com/api/token',
     headers: {
-      'Authorization': `Basic ${basic}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-    }
-  }
-
-  let token;
-  const req = request(options, (err, res) => {
-    if(err) return console.error(err)
-    const resData = JSON.parse(res.body)
-    token = resData.access_token
-
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token
+    })
   })
-  req.write(data);
-  req.end()
-  return token
+
+  return res.json()
 }
 
-export function SpotifyAlbumWidget({albumId}:{albumId: string}) {
+export async function albumRequest(albumId:string) {
+  const {access_token} = await getAccessToken()
+  console.log('=== ==== ===')
   https.get(`https://api.spotify.com/v1/albums/${albumId}`, {
     method: 'GET',
     headers: {
@@ -60,40 +46,43 @@ export function SpotifyAlbumWidget({albumId}:{albumId: string}) {
     }
   }, (res: any) => {
     let data: any = '';
-    res.on('data', (chunk: any) => {
-      data += chunk;
-    })
+    res.on('data', (chunk: any) => { data += chunk; })
     res.on('end', () => {
       let d = JSON.parse(data)
-      var albumArt = d.images[1].url,
-      albumName = d.name,
-      artist = d.artists[0].name,
-      url = d.external_urls.spotify
-
-      obj = (
-        <div className={styles.widget}>
-          <img
-            alt={albumName}
-            className={styles.albumArt}
-            src={albumArt}
-            width="128"
-            height="128"
-          />
-          <div className={styles.mid}>
-            <div className={styles.albumInfo}>
-              <p className={styles.title}>{albumName}</p>
-              <p className={styles.artist}>by <b className={styles.artistBold}>{artist}</b></p>
-            </div>
-            <div className={styles.cta}>
-              <a href={url} title="Open in Spotify"><p className={styles.ctaText}>Open in Spotify</p></a>
-            </div>
-          </div>
-          <Logo />
-        </div>
-        )
+      return obj = {
+        albumArt: d.images[1].url,
+        albumName: d.name,
+        artist: d.artists[0].name,
+        url: d.external_urls.spotify
+      }
     })
   }).on('error', (err: string) => { return console.error(err) })
   return obj
+}
+
+export async function SpotifyAlbumWidget({albumId}:{albumId: string}) {
+  const {albumArt, albumName, artist, url} = await albumRequest(albumId)
+  return (
+    <div className={styles.widget}>
+      <img
+        alt={albumName}
+        className={styles.albumArt}
+        src={albumArt}
+        width="128"
+        height="128"
+      />
+      <div className={styles.mid}>
+        <div className={styles.albumInfo}>
+          <p className={styles.title}>{albumName}</p>
+          <p className={styles.artist}>by <b className={styles.artistBold}>{artist}</b></p>
+        </div>
+        <div className={styles.cta}>
+          <a href={url} title="Open in Spotify"><p className={styles.ctaText}>Open in Spotify</p></a>
+        </div>
+      </div>
+      <Logo />
+    </div>
+  )
 }
 
 export function SpotifyTrackWidget({albumId, token}:{albumId: string, token: string}) {
