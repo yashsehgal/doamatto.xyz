73ab2148271df53d62d71f8652dd2a1fd824a88a
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-04 16:18:29 -0800

    chore: draft 404

---
+++ pages/404.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import { NextSeo } from 'next-seo'
+
+import dynamic from 'next/dynamic'
+const Header = dynamic(() => import('@/components/header'))
+
+export default function Home () {
+  return (
+    <>
+      <NextSeo
+        title="404 - doamatto"
+        description="404 - Horiffic, no good, very bad error has occured."
+      />
+      <Head>
+        <title>404 - doamatto</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container four-oh-four">
+        <img src="/assets/404.svg" />
+        <div>
+          <h1>This page is dead!</h1>
+          <p>
+            Oh god! You just found a dead page. It's possible there was someone else who killed the page. It's also possible we're having a misunderstanding and someone spelt the address wrong. Regardless, we might want to investigate this dead page.
+          </p>
+          <a href="https://git.doamatto.xyz/404-investigation" className="button" title="Investigate the Dead Page">
+            Investigate the Dead Page.
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
