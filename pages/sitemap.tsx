615590af7919eadfc3bdf6823e26956c8f4082ce
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-07 12:50:28 -0800

    chore: added placeholder sitemap

---
+++ pages/sitemap.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {NextSeo} from 'next-seo'
+
+import dynamic from 'next/dynamic'
+const Header = dynamic(() => import('@/components/header'))
+
+export default function Sitemap() {
+  return(
+    <>
+    <NextSeo 
+      title="Sitemap - doamatto"
+      description = "A slew of pages that are available on the site."
+    />
+    <div>
+      <Header title="Sitemap" />
+      <div className="posts" id="mainContent">
+        <p>So far, there is only an XML version of the sitemap.An HTML version is coming soon™.</p>
+        <a
+          href="/sitemap.xml"
+          title="Go to XML sitemap"
+          className="button">
+            Go to XML sitemap
+        </a>
+      </div>
+    </div>
+    </>
+  )
+}
