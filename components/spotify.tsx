dd9defc017086ec7771d9b5b4f0e5bd0a8739859
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-12 10:52:57 -0800

    chore: minimized to SWR for Spotify

--- components/spotify.tsx
+++ components/spotify.tsx
@@ -1,80 +1,30 @@
 import React from 'react'
-import fetch from 'isomorphic-unfetch'
-import querystring from 'querystring'
+import useSWR from 'swr';
+import fetcher from '@/lib/fetcher';
 
 import {default as Logo} from './logos/spotify'
 import styles from './spotify.module.scss'
 
 export default function Main() { return console.error("You need to specify one of the widgets to use this"); }
 
-const {
-  SPOTIFY_CLIENT_ID: client_id,
-  SPOTIFY_CLIENT_SECRET: client_secret,
-  SPOTIFY_REFRESH_TOKEN: refresh_token,
-} = process.env
-
-const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
-export async function getAccessToken() {
-  const res = await fetch("https://accounts.spotify.com/api/token", {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${basic}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: querystring.stringify({
-      grant_type: 'refresh_token',
-      refresh_token
-    })
-  })
-  return res.json()
-}
-
-export const albumRequest = async ({albumId}:{albumId:string}) => {
-  const {access_token} = await getAccessToken()
-  const res = await fetch(`https://api.spotify.com/v1/albums/${albumId}`, {
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': `Bearer ${access_token}`,
-      'Content-Type': 'application/json'
-    }
-  })
-  return res.json()
-}
-
-export const trackRequest = async ({trackId}:{trackId:string}) => {
-  const {access_token} = await getAccessToken()
-  const res = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': `Bearer ${access_token}`,
-      'Content-Type': 'application/json'
-    }
-  })
-  return res.json()
-}
-
-export async function SpotifyAlbumWidget({albumId}:{albumId: string}) {
-  const res = await albumRequest({albumId})
-  const albumArt = res.images[1].url,
-        albumName = res.name,
-        artist = res.artists[0].name,
-        url = res.external_urls.spotify
+export function SpotifyAlbumWidget({albumId}:{albumId: string}) {
+  const { data } = useSWR(`/api/spotify/album?albumId=${albumId}`, fetcher)
   return (
     <div className={styles.widget}>
       <img
-        alt={albumName}
+        alt={data?.albumName}
         className={styles.albumArt}
-        src={albumArt}
+        src={data?.albumArt}
         width="128"
         height="128"
       />
       <div className={styles.mid}>
         <div className={styles.albumInfo}>
-          <p className={styles.title}>{albumName}</p>
-          <p className={styles.artist}>by <b className={styles.artistBold}>{artist}</b></p>
+          <p className={styles.title}>{data?.albumName}</p>
+          <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist}</b></p>
         </div>
         <div className={styles.cta}>
-          <a href={url} title="Open in Spotify">
+          <a href={data?.url} title="Open in Spotify">
             <p className={styles.ctaText}>Open in Spotify</p>
           </a>
         </div>
@@ -84,28 +34,23 @@ export async function SpotifyAlbumWidget({albumId}:{albumId: string}) {
   )
 }
 
-export async function SpotifyTrackWidget({trackId}:{trackId: string}) {
-  const res = await trackRequest({trackId})
-  var albumArt = res.album.images[1].url,
-      albumName = res.album.name,
-      title = res.name,
-      artist = res.artists[0].name,
-      url = res.external_urls.spotify
+export function SpotifyTrackWidget({trackId}:{trackId: string}) {
+  const { data } = useSWR(`/api/spotify/album?trackId=${trackId}`, fetcher)
   return (
     <div className={styles.widget}>
-      <img src={albumArt}
-           alt={albumName}
+      <img src={data?.albumArt}
+           alt={data?.albumName}
            width="128"
            height="128"
            className={styles.albumArt}
       />
       <div className={styles.mid}>
         <div className={styles.albumInfo}>
-          <p className={styles.title}>{title}</p>
-          <p className={styles.artist}>{artist}</p>
+          <p className={styles.title}>{data?.title}</p>
+          <p className={styles.artist}>{data?.artist}</p>
         </div>
         <div className={styles.cta}>
-          <a href={url} title="Open in Spotify">
+          <a href={data?.url} title="Open in Spotify">
             <p className={styles.ctaText}>Open in Spotify</p>
           </a>
         </div>
