b8ccca4906404e49851e84bb99c880898adfb7cc
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-12 11:04:11 -0800

    chore: changed method of reading data in spotify api

--- pages/api/spotify/album.tsx
+++ pages/api/spotify/album.tsx
@@ -2,14 +2,14 @@ import { getAlbum } from '@/lib/spotify'
 
 export default async (req, res) => {
   const response = await getAlbum(req.query.albumId)
-  const { items } = await response.json()
+  const data = await response.json()
 
-  const albumData = items.slice(0, 10).map((data) => ({
-    artist: data.artists.map((_artist) => _artist.name).join(', '),
+  const albumData = {
+    artist: data.artists[0].name,
     url: data.external_urls.spotify,
     albumName: data.name,
     albumArt: data.images[1].url
-  }))
+  }
 
   res.setHeader(
     'Cache-Control',
