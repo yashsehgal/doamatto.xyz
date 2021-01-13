124f541cdfe20b734cf7a242527fcc5034a52a9a
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-13 07:38:02 -0800

    chore: fixed spotify albums and tracks (resolves #82)

--- pages/api/spotify/album.tsx
+++ pages/api/spotify/album.tsx
@@ -1,20 +1,19 @@
 import { getAlbum } from '@/lib/spotify'
 
 export default async (req, res) => {
-  const response = await getAlbum(req.query.albumId)
+  let albumId = await req.query.albumId
+  const response = await getAlbum({albumId})
   const data = await response.json()
-
-  const albumData = {
-    artist: data.artists[0].name,
-    url: data.external_urls.spotify,
-    albumName: data.name,
-    albumArt: data.images[1].url
-  }
 
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=60, stale-while-revalidate=30'
   );
 
-  return res.status(200).json({ albumData })
+  return res.status(200).json({
+    artist: data.artists[0].name,
+    url: data.external_urls.spotify,
+    albumName: data.name,
+    albumArt: data.images[1].url
+  })
 };
--- pages/api/spotify/track.tsx
+++ pages/api/spotify/track.tsx
@@ -1,21 +1,20 @@
 import { getTrack } from '@/lib/spotify'
 
 export default async (req, res) => {
-  const response = await getTrack(req.query.trackId)
+  const trackId = await req.query.trackId
+  const response = await getTrack({trackId})
   const data = await response.json()
-
-  const trackData = {
-    albumArt: data.album.images[1].url,
-    albumName: data.album.name,
-    title: data.name,
-    artist: data.artists[0].name,
-    url: data.external_urls.spotify
-  }
 
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=60, stale-while-revalidate=30'
   );
 
-  return res.status(200).json({ trackData })
+  return res.status(200).json({
+    albumArt: data.album.images[1].url,
+    albumName: data.album.name,
+    title: data.name,
+    artist: data.artists[0].name,
+    url: data.external_urls.spotify
+  })
 };
