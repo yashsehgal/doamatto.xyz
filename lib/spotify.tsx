7fe1a38c42702cb2efaba42655d7a2b9218b7b2e
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-13 07:37:24 -0800

    fix()

--- lib/spotify.tsx
+++ lib/spotify.tsx
@@ -1,13 +1,13 @@
-import fetch from 'isomorphic-unfetch';
-import querystring from 'querystring';
+import fetch from 'isomorphic-unfetch'
+import querystring from 'querystring'
 
 const {
   SPOTIFY_CLIENT_ID: client_id,
   SPOTIFY_CLIENT_SECRET: client_secret,
   SPOTIFY_REFRESH_TOKEN: refresh_token
-} = process.env;
+} = process.env
 
-const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
 
 const getAccessToken = async () => {
   const res = await fetch("https://accounts.spotify.com/api/token", {
@@ -22,24 +22,23 @@ const getAccessToken = async () => {
     })
   })
   return res.json()
-};
+}
 
 export const getAlbum = async ({albumId}:{albumId:string}) => {
-  const { access_token } = await getAccessToken();
-
+  const { access_token } = await getAccessToken()
   return fetch(`https://api.spotify.com/v1/albums/${albumId}`, {
     headers: {
       Authorization: `Bearer ${access_token}`
     }
-  });
-};
+  })
+}
 
 export const getTrack = async ({trackId}:{trackId:string}) => {
-  const { access_token } = await getAccessToken();
+  const { access_token } = await getAccessToken()
 
   return fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
     headers: {
       Authorization: `Bearer ${access_token}`
     }
-  });
-};
+  })
+}
