45bb837c0c27e4407807c3c948021441049d6f4d
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-13 08:09:13 -0800

    fix: centre of spotify widgets

--- components/spotify.tsx
+++ components/spotify.tsx
@@ -10,26 +10,28 @@ export default function Main() { return console.error("You need to specify one o
 export function SpotifyAlbumWidget({albumId}:{albumId: string}) {
   const { data } = useSWR(`/api/spotify/album?albumId=${albumId}`, fetcher)
   return (
-    <div className={styles.widget}>
-      <img
-        alt={data?.albumName ?? 'Error fetching data'}
-        className={styles.albumArt}
-        src={data?.albumArt ?? '/assets/placeholder.jpg'}
-        width="128"
-        height="128"
-      />
-      <div className={styles.mid}>
-        <div className={styles.albumInfo}>
-          <p className={styles.title}>{data?.albumName ?? 'Couldn\'t find album data'}</p>
-          <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist ?? 'an error that could\'ve been prevented'}</b></p>
+    <div id={data?.id} className={styles.wdgtCntanr}>
+      <div className={styles.widget}>
+        <img
+          alt={data?.albumName ?? 'Error fetching data'}
+          className={styles.albumArt}
+          src={data?.albumArt ?? '/assets/placeholder.jpg'}
+          width="128"
+          height="128"
+        />
+        <div className={styles.mid}>
+          <div className={styles.albumInfo}>
+            <p className={styles.title}>{data?.albumName ?? 'Couldn\'t find album data'}</p>
+            <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist ?? 'an error that could\'ve been prevented'}</b></p>
+          </div>
+          <div className={styles.cta}>
+            <a href={data?.url} title="Open in Spotify">
+              <p className={styles.ctaText}>Open in Spotify</p>
+            </a>
+          </div>
         </div>
-        <div className={styles.cta}>
-          <a href={data?.url} title="Open in Spotify">
-            <p className={styles.ctaText}>Open in Spotify</p>
-          </a>
-        </div>
+        <Logo />
       </div>
-      <Logo />
     </div>
   )
 }
@@ -37,25 +39,27 @@ export function SpotifyAlbumWidget({albumId}:{albumId: string}) {
 export function SpotifyTrackWidget({trackId}:{trackId: string}) {
   const { data } = useSWR(`/api/spotify/album?trackId=${trackId}`, fetcher)
   return (
-    <div className={styles.widget}>
-      <img src={data?.albumArt ?? '/assets/placeholder.jpg'}
-           alt={data?.albumName ?? 'Error fetching data'}
-           width="128"
-           height="128"
-           className={styles.albumArt}
-      />
-      <div className={styles.mid}>
-        <div className={styles.albumInfo}>
-          <p className={styles.title}>{data?.title ?? 'Couldn\'t fetch track data'}</p>
-          <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist ?? 'an error that could\'ve been prevented'}</b></p>
+    <div id={data?.id} className={styles.wdgtCntanr}>
+      <div className={styles.widget}>
+        <img src={data?.albumArt ?? '/assets/placeholder.jpg'}
+            alt={data?.albumName ?? 'Error fetching data'}
+            width="128"
+            height="128"
+            className={styles.albumArt}
+        />
+        <div className={styles.mid}>
+          <div className={styles.albumInfo}>
+            <p className={styles.title}>{data?.title ?? 'Couldn\'t fetch track data'}</p>
+            <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist ?? 'an error that could\'ve been prevented'}</b></p>
+          </div>
+          <div className={styles.cta}>
+            <a href={data?.url} title="Open in Spotify">
+              <p className={styles.ctaText}>Open in Spotify</p>
+            </a>
+          </div>
         </div>
-        <div className={styles.cta}>
-          <a href={data?.url} title="Open in Spotify">
-            <p className={styles.ctaText}>Open in Spotify</p>
-          </a>
-        </div>
+        <Logo />
       </div>
-      <Logo />
     </div>
   )
 }
