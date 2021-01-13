1b0895ef32dd131ad3f813fda63876f64614b721
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-13 07:37:13 -0800

    chore: added placeholders for when data can't fetch

--- components/spotify.tsx
+++ components/spotify.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import useSWR from 'swr';
-import fetcher from '@/lib/fetcher';
+import useSWR from 'swr'
+import fetcher from '@/lib/fetcher'
 
 import {default as Logo} from './logos/spotify'
 import styles from './spotify.module.scss'
 
-export default function Main() { return console.error("You need to specify one of the widgets to use this"); }
+export default function Main() { return console.error("You need to specify one of the widgets to use this") }
 
 export function SpotifyAlbumWidget({albumId}:{albumId: string}) {
   const { data } = useSWR(`/api/spotify/album?albumId=${albumId}`, fetcher)
   return (
     <div className={styles.widget}>
       <img
-        alt={data?.albumName}
+        alt={data?.albumName ?? 'Error fetching data'}
         className={styles.albumArt}
-        src={data?.albumArt}
+        src={data?.albumArt ?? '/assets/placeholder.jpg'}
         width="128"
         height="128"
       />
       <div className={styles.mid}>
         <div className={styles.albumInfo}>
-          <p className={styles.title}>{data?.albumName}</p>
-          <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist}</b></p>
+          <p className={styles.title}>{data?.albumName ?? 'Couldn\'t find album data'}</p>
+          <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist ?? 'an error that could\'ve been prevented'}</b></p>
         </div>
         <div className={styles.cta}>
           <a href={data?.url} title="Open in Spotify">
@@ -38,16 +38,16 @@ export function SpotifyTrackWidget({trackId}:{trackId: string}) {
   const { data } = useSWR(`/api/spotify/album?trackId=${trackId}`, fetcher)
   return (
     <div className={styles.widget}>
-      <img src={data?.albumArt}
-           alt={data?.albumName}
+      <img src={data?.albumArt ?? '/assets/placeholder.jpg'}
+           alt={data?.albumName ?? 'Error fetching data'}
            width="128"
            height="128"
            className={styles.albumArt}
       />
       <div className={styles.mid}>
         <div className={styles.albumInfo}>
-          <p className={styles.title}>{data?.title}</p>
-          <p className={styles.artist}>{data?.artist}</p>
+          <p className={styles.title}>{data?.title ?? 'Couldn\'t fetch track data'}</p>
+          <p className={styles.artist}>by <b className={styles.artistBold}>{data?.artist ?? 'an error that could\'ve been prevented'}</b></p>
         </div>
         <div className={styles.cta}>
           <a href={data?.url} title="Open in Spotify">
