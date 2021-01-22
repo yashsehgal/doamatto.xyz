e07e47cadc3f76961d214b789d3818361cfa68b7
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-01-22 08:11:11 -0800

    chore: adde duses icons (not styled properly)

--- components/item.tsx
+++ components/item.tsx
@@ -1,22 +1,40 @@
 const styles = require('./item.module.scss')
+import Image from 'next/image'
 
-export default function Item ({title,titleUrl,description,url}: {
+export default function Item ({title,titleUrl,description,imageId,url}: {
   title: string
   titleUrl?: string // optional
+  imageId?: string // optional
   description: string
   url: string
 }) {
   return (
     <section className={styles.item}>
       <a href={url} title={title}>
-        <TitleGen title={title} titleUrl={titleUrl} />
-        <p>{description}</p>
+        <Icon title={title} imageId={imageId} />
+        <div className={styles.text}>
+          <TitleGen title={title} titleUrl={titleUrl} />
+          <p>{description}</p>
+        </div>
       </a>
     </section>
   )
 }
 
-function TitleGen({title, titleUrl}) {
+function Icon({title,imageId}: {title:string, imageId: string}) {
+  if(imageId !== undefined) return (
+    <Image
+      src={`/assets/uses/${imageId}.png`}
+      alt={title}
+      width={64}
+      height={64}
+      className="uses_icon"
+    />
+  )
+  return null
+}
+
+function TitleGen({title, titleUrl}:{title:string,titleUrl:string}) {
   if(titleUrl !== undefined) return (
     <a href={titleUrl} title={title}>
         <h3 className={styles.itemTitle}>{title}</h3>
