848971676740224fdc3448af1a1185b56e9ae67d
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-02-08 21:30:49 -0800

    chore: added dashboard items (unstyled; unfinished)

---
+++ components/dashboard-item.tsx
@@ -0,0 +1,37 @@
+const styles = require('./dashboard-item.module.scss')
+
+export default function dashboardItem ({
+  value,
+  caption,
+  link,
+  service
+}: {
+  value : string,
+  caption : string,
+  link : string,
+  service : string
+}) {
+  return (
+    <a href={link} rel={service}>
+      <section>
+        <p className={styles.metric}>{value}</p>
+        <p className={styles.caption}>{caption}<LinkIcon/></p>
+      </section>
+    </a>
+  )
+}
+
+function LinkIcon() {
+  return (
+    <svg className={styles.linkIcon}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor">
+      <path strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
+    </svg>
+  )
+}
