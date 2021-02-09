63c87caffcd615af25430f80d52b14d62d864070
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-02-08 18:17:15 -0800

    fix: authentication for Unsplash

--- lib/unsplash.ts
+++ lib/unsplash.ts
@@ -2,7 +2,11 @@ import fetch from 'isomorphic-unfetch'
 
 export const fetchData = async () => {
   const user = await (
-    await fetch('https://unsplash.com/users/doamatto')
+    await fetch('https://api.unsplash.com/users/doamatto', {
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASHKEY}`
+      }
+    })
   ).json()
 
   return {
