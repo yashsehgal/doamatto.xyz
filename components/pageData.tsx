348bc2cc35d82f51dd32c5a6df15b9ab6880c0a3
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-03-16 18:29:42 -0700

    fix: sonarcloud dupes

--- components/pageData.tsx
+++ components/pageData.tsx
@@ -5,7 +5,7 @@ export default function pageData ({ children, frontMatter }: {children: any, fro
   const date = frontMatter.date
   return (
       <>
-        <div className="content">
+        <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl content">
           <h1 className="header">{title}</h1>
           <p className="minor">{frontMatter.readingTime.text}{' • '}{format(parseISO(date), 'MMMM dd, yyyy')}</p>
           <section>
