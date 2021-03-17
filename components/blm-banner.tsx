8e79d84ac2f78eb1c5a60e1efb222ec90822cc8c
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-03-16 18:20:35 -0700

    chore: convert BLM banner to tailwind

--- components/blm-banner.tsx
+++ components/blm-banner.tsx
@@ -1,8 +1,10 @@
-const styles = require('./blm-banner.module.scss')
-
 export default function BLMBanner () {
   return (
-        <a className={styles.banner} href="https://support.eji.org/give/153413/#!/donation/checkout" title="Support the Equal Justice Initiative">
+        <a
+          className="h-12 p-1 bg-black text-center flex items-center justify-center text-white font-extrabold no-underline text-base
+          border-b border-white transition-colors duration-300 hover:text-indigo-400 hover:border-indigo-400"
+          href="https://support.eji.org/give/153413/#!/donation/checkout"
+          title="Support the Equal Justice Initiative">
             Black Lives Matter. Support&nbsp;the&nbsp;Equal&nbsp;Justice&nbsp;Initiative.
         </a>
   )
