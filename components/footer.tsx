35b4a1b8c6c1a179536c13f6784fca882c099b36
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-03-16 18:20:42 -0700

    chore: convert footer to tailwind

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,12 @@
-const styles = require('./footer.module.scss')
-// import {useRouter} from 'next/router'
-// import Link from 'next/link'
-
-// TODO: Switch to <Link> for all links in footer
+import Link from 'next/link'
 
 export default function Footer () {
   return (
-        <footer className={styles.footer}>
+        <footer className="text-center">
             <p>
                 &copy; 2019-2021 doamatto. <br/>
-                <a href={'https://github.com/doamatto/doamatto.xyz'}>View this page&apos;s source code.</a> <br/>
-                <a href="/imprint">Imprint.</a> <a href="/privacy">Privacy.</a> <a href="/sitemap">Sitemap.</a> <br/>
-                {/* <p>
-                  View this page in: <Link href={`/${router.pathname}`} locale={'en'}><a>English</a></Link> <Link href={`/fr/${router.pathname}`} locale={'fr'}><a>Français</a></Link><Link href={`/jp/${router.pathname}`} locale={'jp'}><a>日本語</a></Link>
-                </p> */}
+                <Link href='https://github.com/doamatto/doamatto.xyz'>View this page&apos;s source code.</Link> <br/>
+                <Link href="/imprint">Imprint.</Link> <Link href="/privacy">Privacy.</Link> <Link href="/sitemap">Sitemap.</Link> <br/>
             </p>
         </footer>
   )
