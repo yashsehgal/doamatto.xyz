2c284a542c35c3b73473cbd5febe9d1a717ee12e
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-03-17 09:50:21 -0700

    chore: theming of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     enabled: true,
@@ -11,8 +14,56 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'lightPurple': 'var(--light-purple)',
+        'darkPurple': 'var(--dark-purple)',
+        'lightAlt': 'var(--light-alt)',
+        'darkAlt': 'var(--dark-alt)',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h1: { color: 'white' },
+            color: 'white',
+            a: {
+              textDecoration: 'none',
+              borderBottom: '1px solid',
+              borderColor: theme('colors.white'),
+              transition: 'border-color 250ms, color 250ms',
+              color: 'inherit',
+              '&:hover': {
+                color: theme('colors.lightPurple'),
+                borderColor: theme('colors.lightPurple')
+              }
+            },
+            ul: { li: { '&:before': { backgroundColor: theme('colors.gray.600') } } },
+            'h2,h3,h4': {
+              color: theme('colors.gray.50'),
+              'scroll-margin-top': spacing[32]
+            },
+          }
+        },
+        dark: {
+          css: {
+            color: 'black',
+            a: {
+              borderColor: 'black',
+              '&:hover': {
+                color: theme('colors.darkPurple'),
+                borderColor: theme('colors.darkPurple')
+              }
+            },
+            hr: { borderColor: theme('colors.gray.800') },
+            ul: { li: { '&:before': { backgroundColor: theme('colors.gray.600') } } },
+            'h2,h3,h4': {
+              color: theme('colors.gray.100'),
+              'scroll-margin-top': spacing[32]
+            },
+          }
+        }
+      }),
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif']
+        sans: ['Inter', ...fontFamily.sans]
       }
     }
   },
