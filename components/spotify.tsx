3b4fe84beeb69517960b3144faffb89df8beb68a
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-03-17 09:49:58 -0700

    chore: add mini widget

--- components/spotify.tsx
+++ components/spotify.tsx
@@ -65,6 +65,23 @@ export function SpotifyTrackWidget ({ trackId }:{trackId: string}) {
   )
 }
 
+export function SpotifyMiniAlbumWidget ({albumId}:{albumId: string}) {
+  const { data } = useSWR(`/api/spotify/album?albumId=${albumId}`, fetcher)
+  return (
+    <div id={data?.id} className="w-32 h-32 m-2">
+      <a href={data?.url}>
+        <img
+          src={data?.albumArt ?? '/assets/placeholder.jpg'}
+          alt={data?.albumName ?? 'Error fetching data'}
+          width={128}
+          height={128}
+          className="rounded-2xl"
+        />
+      </a>
+    </div>
+  )
+}
+
 export function SpotifyNowPlayingWidget () {
   const { data } = useSWR('/api/spotify/now-playing', fetcher)
   return (
