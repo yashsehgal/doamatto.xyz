6058a849891db2e559b6ef79600b30492d1bed60
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-03-16 18:20:25 -0700

    deps: installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './pages/**/*.{ts,tsx}',
+      './components/**/*.{ts,tsx}',
+      './layouts/**/*.{ts,tsx}',
+      './lib/**/*.{ts,tsx}'
+    ]
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif']
+      }
+    }
+  },
+  plugins: [ require('@tailwindcss/typography') ],
+}
