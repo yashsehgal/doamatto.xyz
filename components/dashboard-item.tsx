1f0b3e1d23d610ae47286e1cc433719fd33d6ab3
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-04-01 15:10:19 -0700

    fix: remove xmlns and pray it still works

--- components/dashboard-item.tsx
+++ components/dashboard-item.tsx
@@ -19,7 +19,6 @@ export default function dashboardItem ({
           {caption}
           <svg
             className="h-4 w-4 ml-1"
-            xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
