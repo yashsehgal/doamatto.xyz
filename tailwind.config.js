e96e88577063c8597361753ec2e98c80d5256a6b
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-04-07 16:22:06 -0700

    pr: prep for #110 (closes #101)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const { spacing, fontFamily } = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  mode: 'jit',
   purge: {
     enabled: true,
     content: [
