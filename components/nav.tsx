eb94cb80e65d452e208bea3c083c6ff1d5aad105
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-04-09 12:00:29 -0700

    chore: started partial conversion to tailwind

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,28 +4,29 @@ const styles = require('./nav.module.scss')
 export default function nav () {
   return (
       <>
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <a href="/" rel="Home button" title="Home">
+        {/* Sticky nav backdrop from Lee Robinso: https://leerob.io */}
+        <header className={styles.header + " text-center flex justify-between -ml-8 pl-8 sticky z-10 top-0 backdrop-filter backdrop-blur-xl backdrop-saturate-180"}>
+            <div className="m-0 flex justify-center">
+                <a href="/" className="border-none group transition-colors" rel="Home button" title="Home">
                     {
                     <Logo />
                     }
                 </a>
             </div>
-            <nav className={styles.nav}>
-                <ul>
-                    <li>
-                        <a href="/blog" title="Blog">
+            <nav className={styles.nav + "  flex items-center align-middle justify-center"}>
+                <ul className="p-0 flex text-center list-none">
+                    <li className="m-0 p-1 flex flex-col justify-center">
+                        <a href="/blog" title="Blog" className="border-none p-1 text-lg my-0 mx-3.5 transition- no-underline">
                             Blog
                         </a>
                     </li>
-                    <li>
-                        <a href="/projects" title="Projects">
+                    <li className="m-0 p-1 flex flex-col justify-center">
+                        <a href="/projects" title="Projects" className="border-none p-1 text-lg my-0 mx-3.5 transition- no-underline">
                             Projects
                         </a>
                     </li>
-                    <li>
-                        <a href="/about" title="About">
+                    <li className="m-0 p-1 flex flex-col justify-center">
+                        <a href="/about" title="About" className="border-none p-1 text-lg my-0 mx-3.5 transition- no-underline">
                             About
                         </a>
                     </li>
